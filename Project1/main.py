a290e595ebc0336bf01f6e346a7266ccd185f89a
brick-mover <shaojiuru@ucla.edu>
2018-01-20 11:14:05 -0800

    tfidf

--- Project1/main.py
+++ Project1/main.py
@@ -1,19 +1,44 @@
 from sklearn.datasets import fetch_20newsgroups
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+
+
 
 categories = [ 'comp.graphics', 'comp.os.ms-windows.misc', 'comp.sys.ibm.pc.hardware', 'comp.sys.mac.hardware',
                'rec.autos', 'rec.motorcycles', 'rec.sport.baseball', 'rec.sport.hockey' ]
 
-#trainingData = fetch_20newsgroups(subset='train', categories=categories, shuffle=True)
 
-
+"""
+Q1: plot a histogram of the number of training documents per class to check if they are evenly distributed.
+"""
 def plot_size():
     for category in categories:
         trainingData = fetch_20newsgroups(subset='train', categories=[category])
-        print(category, trainingData.filenames.shape[0])
+        print(category, len(trainingData.filenames))
+
+
+"""
+Modeling Text Data: tokenize each document into words. Then, excluding the stop words,
+punctuations, and using stemmed version of words, create a TFxIDF vector representations.
+"""
+def model_text_data():
+    # load data
+    eightTrainData = fetch_20newsgroups(subset='train', categories=categories, shuffle=True)
+
+    # tokenization
+    countVec = CountVectorizer()
+    XTrainCounts = countVec.fit_transform(eightTrainData.data)
+    print('Size of feature vectors:', XTrainCounts.shape)
+
+    # compute tf-idf
+    tfidfTransformer = TfidfTransformer()
+    XTrainTfidf= tfidfTransformer.fit_transform(XTrainCounts)
+    print('Size of tf-idf:', XTrainTfidf.shape)
+    print(XTrainTfidf[:2,:])
 
 def main():
     #plot_size()
-    pass
+    model_text_data()
 
 if __name__ == "__main__":
     main()
