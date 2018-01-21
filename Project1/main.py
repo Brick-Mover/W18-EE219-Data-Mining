1e6d299645f4df91773f0a0991326cb399e9fa47
brick-mover <shaojiuru@ucla.edu>
2018-01-20 20:54:03 -0800

    add args

--- Project1/main.py
+++ Project1/main.py
@@ -7,40 +7,62 @@ from sklearn.feature_extraction import text
 categories = [ 'comp.graphics', 'comp.os.ms-windows.misc', 'comp.sys.ibm.pc.hardware', 'comp.sys.mac.hardware',
                'rec.autos', 'rec.motorcycles', 'rec.sport.baseball', 'rec.sport.hockey' ]
 
+class Project1(object):
 
-"""
-Q1: plot a histogram of the number of training documents per class to check if they are evenly distributed.
-"""
-def plot_size():
-    for category in categories:
-        trainingData = fetch_20newsgroups(subset='train', categories=[category])
-        print(category, len(trainingData.filenames))
+    def __init__(self, minDf):
+        self.eightTrainingData = None
+        self.minDf = minDf
+
+    """
+    (a) Plot a histogram of the number of training documents per class to check if they are evenly distributed.
+    """
+    def problemA(self):
+        self.plot_size()
+
+    def plot_size(self):
+        for category in categories:
+            trainingData = fetch_20newsgroups(subset='train', categories=[category])
+            print(category, len(trainingData.filenames))
 
 
-"""
-Modeling Text Data: tokenize each document into words. Then, excluding the stop words,
-punctuations, and using stemmed version of words, create a TFxIDF vector representations.
-min_df = 2 or 5
-"""
+    """
+    (b) Modeling Text Data: tokenize each document into words. Then, excluding the stop words,
+    punctuations, and using stemmed version of words, create a TFxIDF vector representations.
+    min_df = 2 or 5
+    """
+    def problemB(self):
+        self.model_text_data()
+
+    def model_text_data(self):
+        # load data
+        if not self.eightTrainingData:
+            self.eightTrainingData = fetch_20newsgroups(subset='train', categories=categories, shuffle=True)
+
+        # tokenization
+        countVec = CountVectorizer(min_df=self.minDf, stop_words=text.ENGLISH_STOP_WORDS)
+        XTrainCounts = countVec.fit_transform(self.eightTrainingData.data)
+        print('Size of feature vectors when minDf is %s: %s' % (self.minDf, XTrainCounts.shape))
+
+        # compute tf-idf
+        tfidfTransformer = TfidfTransformer()
+        XTrainTfidf = tfidfTransformer.fit_transform(XTrainCounts)
+        print('Size of tf-idf when minDf is %s: %s' % (self.minDf, XTrainTfidf.shape))
 
 
-def model_text_data(minDf):
-    # load data
-    eightTrainData = fetch_20newsgroups(subset='train', categories=categories, shuffle=True)
+    """
+    (c) Report 10 most significant terms base on tf-icf
+    """
+    def problemC(self):
+        self.calc_tf_icf()
 
-    # tokenization
-    countVec = CountVectorizer(min_df=minDf, stop_words=text.ENGLISH_STOP_WORDS)
-    XTrainCounts = countVec.fit_transform(eightTrainData.data)
-    print('Size of feature vectors when minDf is %d: %d' % (minDf, XTrainCounts.shape))
+    def calc_tf_icf(self):
+        pass
 
-    # compute tf-idf
-    tfidfTransformer = TfidfTransformer()
-    XTrainTfidf= tfidfTransformer.fit_transform(XTrainCounts)
-    print('Size of tf-idf when minDf is %d: %d' % (minDf, XTrainTfidf.shape))
+
 
 def main():
-    #plot_size()
-    model_text_data()
+    p = Project1(minDf=2)
+    p.problemB()
 
 if __name__ == "__main__":
     main()
