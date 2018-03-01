09891a56a4890f8d800b3c00a21b5477ad14ca74
echen94 <chenyiying@ucla.edu>
2018-02-28 23:28:32 -0800

    added 2aiii

--- Project4/Project4.py
+++ Project4/Project4.py
@@ -7,154 +7,219 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error
 from math import sqrt
 from sklearn import feature_selection
-from sklearn.feature_selection import VarianceThreshold
+from sklearn.feature_selection import VarianceThreshold, f_regression, mutual_info_regression, SelectKBest
+from sklearn.ensemble import RandomForestRegressor
 
+
+"""
+CONSTANTS HERE
+"""
+N_WORKFLOW = 5
+N_FILE = 30
+N_DAY = 7
+N_WEEK = 15
+N_HOUR = 24
 
 
 data_frame = pd.read_csv("./data/network_backup_dataset.csv")
 column_names = data_frame.columns
-counter = 0;
+counter = 0
 print("column index --- name:")
 for i in column_names:
-	print("           " + str(counter) + " --- "+ i)
-	counter = counter + 1
+    print("           " + str(counter) + " --- "+ i)
+    counter = counter + 1
 
 def get_date(week_num, day_num):
-	weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-	return ((int(week_num) - 1) * 7 + weekdays.index(day_num))
+    weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    return ((int(week_num) - 1) * 7 + weekdays.index(day_num))
 
 
 prepare_data = dict() # {"work_flow_id": {"file_name": [0,0,....,0]}}
 
 for label_list, row_info in data_frame.groupby([column_names[3],column_names[4]]):
-	work_flow_id = label_list[0]
-	file_name = label_list[1]
+    work_flow_id = label_list[0]
+    file_name = label_list[1]
 
-	if work_flow_id not in prepare_data:
-		prepare_data[work_flow_id] = dict()
-	if file_name not in prepare_data[work_flow_id]:
-		prepare_data[work_flow_id][file_name] = [0] * 106
+    if work_flow_id not in prepare_data:
+        prepare_data[work_flow_id] = dict()
+    if file_name not in prepare_data[work_flow_id]:
+        prepare_data[work_flow_id][file_name] = [0] * 106
 
-	for week_and_day_list, row in row_info.groupby([column_names[0], column_names[1]]):
-		date = get_date(week_and_day_list[0], week_and_day_list[1])
-		prepare_data[work_flow_id][file_name][date] = sum(row[column_names[5]])
+    for week_and_day_list, row in row_info.groupby([column_names[0], column_names[1]]):
+        date = get_date(week_and_day_list[0], week_and_day_list[1])
+        prepare_data[work_flow_id][file_name][date] = sum(row[column_names[5]])
 
 
 def problem1_plot(period):
-	x = [a for a in range(0,period+1)]
-	for work_flow_id in prepare_data:
-		yaxis = [0] * (period+1)
-		for day in range(0, period+1, 1):
-			totalsize = 0
-			for file_name in prepare_data[work_flow_id]:
-				totalsize = totalsize + prepare_data[work_flow_id][file_name][day]
-			yaxis[day] = totalsize
-		#print(yaxis)
-		#print("------")	
-		plt.plot(x, yaxis, label = "work_flow_ " + str(work_flow_id))
-		plt.title(str(period) + " days period")
-		plt.xlabel('Days')
-		plt.ylabel('Total Size of Backup in GB')
-	plt.legend()
-	plt.show()
+    x = [a for a in range(0, period+1)]
+    for work_flow_id in prepare_data:
+        yaxis = [0] * (period+1)
+        for day in range(0, period+1, 1):
+            totalsize = 0
+            for file_name in prepare_data[work_flow_id]:
+                totalsize = totalsize + prepare_data[work_flow_id][file_name][day]
+            yaxis[day] = totalsize
+        #print(yaxis)
+        #print("------")
+        plt.plot(x, yaxis, label = "work_flow_ " + str(work_flow_id))
+        plt.title(str(period) + " days period")
+        plt.xlabel('Days')
+        plt.ylabel('Total Size of Backup in GB')
+    plt.legend()
+    plt.show()
 
 def Q1(option):
-	if option == 'a':
-		problem1_plot(20)
-	elif option == 'b':
-		problem1_plot(105)
+    if option == 'a':
+        problem1_plot(20)
+    elif option == 'b':
+        problem1_plot(105)
 
-def encode_workflow(workflow):
-	for i in range(len(workflow)):
-		workflow[i] = int (workflow[i].split('_')[-1])
-	return workflow 
 
-def encode_files (files):
-	for i in range(len(files)):
-		files[i] = int (files[i].split('_')[-1])
-	return files 
+def one_hot(total: int, one: int):
+    result = [0] * total
+    result[one] = 1
+    return result
 
-def encode_day(days):
-	week_days = {'Monday' : 1, 'Tuesday' : 2, 'Wednesday' : 3 , 'Thursday' : 4, 'Friday' : 5,
-'Saturday' : 6, 'Sunday' : 7 }
-	for i in range(len(days)):
-		days[i] = week_days ['Monday']
-	return days
 
-def Q2a():
-	X = data_frame.ix [:,[0,1,2,3,4]].values
-	X [:,1] = encode_day(X[:,1])
-	X [:,3] = encode_workflow(X[:,3])
-	X [:,4] = encode_files(X[:,4])
+def encode_workflow(workflow, useOnehot=False):
+    for i in range(len(workflow)):
+        workflow[i] = int(workflow[i].split('_')[-1])
+        if useOnehot:
+            workflow[i] = one_hot(N_WORKFLOW, workflow[i])
+    return workflow
 
-	y = data_frame.ix [:,5].values
-	lr = linear_model.LinearRegression()
 
-	kf = KFold(n_splits=10)
+def encode_files(files, useOnehot=False):
+    for i in range(len(files)):
+        files[i] = int (files[i].split('_')[-1])
+        if useOnehot:
+            files[i] = one_hot(N_FILE, files[i])
+    return files
 
-	rmse_test = []
-	rmse_train = []
 
-	# 10 folds 
-	for train_index, test_index in kf.split(X):
-		X_train, X_test = X[train_index], X[test_index]
-		y_train, y_test = y[train_index], y[test_index]
-		lr.fit(X_train, y_train)
-		y_predicted = lr.predict(X_test)
-		y_predicted_train = lr.predict(X_train)
-		rmse_test.append(sqrt(mean_squared_error(y_test, y_predicted)))
-		rmse_train.append(sqrt(mean_squared_error(y_train, y_predicted_train)))
+def encode_day(days, useOnehot=False):
+    week_days = { 'Monday' : 1,
+                  'Tuesday' : 2,
+                  'Wednesday' : 3,
+                  'Thursday' : 4,
+                  'Friday' : 5,
+                  'Saturday' : 6,
+                  'Sunday' : 7 }
+    for i in range(len(days)):
+        days[i] = week_days[days[i]]
+        if useOnehot:
+            days[i] = one_hot(N_DAY, days[i]-1)
+    return days
 
-	# TODO: need to double check how to report test and train rmse. refer to Piazza 
-	print ('test rmse: ', np.mean(rmse_test))
-	print ('train rmse: ', np.mean(rmse_train))
 
-	# TODO: do we use all data to plot scatterplot? 
-	# plot scatter 
-	lr.fit(X, y)
-	y_predicted = lr.predict(X)
-	fig, ax = plt.subplots()
-	ax.scatter(y, y_predicted, edgecolors=(0, 0, 0))
-	ax.plot([y.min(), y.max()], [y.min(), y.max()], 'k--', lw=4)
-	ax.set_xlabel('Measured')
-	ax.set_ylabel('Predicted')
-	plt.show()
+def getXy(useOnehot=False):
+    X = data_frame.iloc[:,[0,1,2,3,4]].values
+    X[:,1] = encode_day(X[:,1], useOnehot)
+    X[:,3] = encode_workflow(X[:,3], useOnehot)
+    X[:,4] = encode_files(X[:,4], useOnehot)
+    if useOnehot:
+        for i in range(len(X[:0])):
+            X[:0][i] = one_hot(N_WEEK, X[:0][i]-1)
+            X[:2][i] = one_hot(N_HOUR, X[:2][i]-1)
+    y = data_frame.iloc[:,5].values
+    return X,y
 
-	# plot residual 
-	y_residual = y - y_predicted 
-	fig, ax = plt.subplots()
-	ax.scatter(y_predicted, y_residual, edgecolors=(0, 0, 0))
-	ax.set_xlabel('Fitted')
-	ax.set_ylabel('Residual')
-	plt.show()
 
-	# TODO: 
-	# standardize 
-	# standardization shows no change right now. need to fix 
+def cross_val(clf, X, y):
+    kf = KFold(n_splits=10)
 
-	x_scaler = StandardScaler()
-	X_stan = x_scaler.fit_transform(X)
+    # squre root errors sr_test and sr_train
+    sr_test = np.array([])
+    sr_train = np.array([])
+    for train_index, test_index in kf.split(X):
+        X_train, X_test = X[train_index], X[test_index]
+        y_train, y_test = y[train_index], y[test_index]
+        clf.fit(X_train, y_train)
+        y_pred_test = clf.predict(X_test)
+        y_pred_train = clf.predict(X_train)
+        sub_sr_test = pow(np.array(y_test)-np.array(y_pred_test), 2)
+        sr_test = np.append(sr_test, sub_sr_test)
+        sub_sr_train = pow(np.array(y_train)-np.array(y_pred_train), 2)
+        sr_train = np.append(sr_train, sub_sr_train)
 
-	for train_index, test_index in kf.split(X_stan):
-		X_train, X_test = X[train_index], X[test_index]
-		y_train, y_test = y[train_index], y[test_index]
-		lr.fit(X_train, y_train)
-		y_predicted = lr.predict(X_test)
-		y_predicted_train = lr.predict(X_train)
-		rmse_test.append(sqrt(mean_squared_error(y_test, y_predicted)))
-		rmse_train.append(sqrt(mean_squared_error(y_train, y_predicted_train)))
-	print ('test rmse: ', np.mean(rmse_test))
-	print ('train rmse: ', np.mean(rmse_train))
+    rmse_test = sqrt(np.sum(sr_test)/sr_test.size)
+    rmse_train = sqrt(np.sum(sr_train)/sr_train.size)
+    print(sr_test.size, sr_train.size)
+    # TODO: need to double check how to report test and train rmse. refer to Piazza
+    print ('test rmse: ', np.mean(rmse_test))
+    print ('train rmse: ', np.mean(rmse_train))
 
-	fig_stan, ax_stan = plt.subplots()
-	ax_stan.scatter(y_test, y_predicted, edgecolors=(0, 0, 0))
-	ax_stan.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'k--', lw=4)
-	ax_stan.set_xlabel('Measured')
-	ax_stan.set_ylabel('Predicted')
-	plt.show()
+# 
+# ys is [[y, 'label'],...]
+# 
+def scatter(x, ys, xlabel=None, ylabel=None, xticks=None, grid=False, title=None):
+    for y, label in ys:
+        plt.scatter(x, y, s=1, marker='.', label=label)
+    if xlabel is not None:
+        plt.xlabel(xlabel)
+    if ylabel is not None:
+        plt.ylabel(ylabel)
+    if xticks is not None:
+        plt.xticks(x)
+    plt.legend()
+    if grid == True:
+        plt.grid()
+    if title is not None:
+        plt.title(title)
+    plt.show()
+
+def Q2a(option):
+	X,y = getXy()
+
+	if (option == 'i'): 
+		lr = linear_model.LinearRegression()
+		cross_val(lr, X, y)
+
+		# plot scatter 
+		lr.fit(X, y)
+		y_predicted = lr.predict(X)
+		x_plt = [x for x in range(len(y))]
+		title = 'Fitted against ture values'
+		ys = [[y, 'Ture'], [y_predicted, 'Fitted']]
+		fig, ax = plt.subplots()
+		scatter(x_plt, ys, title=title)
+
+		# plot residual 
+		y_residual = y - y_predicted 
+		title = 'Residual against fitted values'
+		ys = [[y_residual, 'Residual'],[y_predicted, 'Fitted']]
+		scatter(x_plt, ys, title=title)
+	elif (option == 'ii'):
+		# standardize 
+		x_plt = [x for x in range(len(y))]
+		x_scaler = StandardScaler()
+		X_stan = x_scaler.fit_transform(X)
+		lr_stan = linear_model.LinearRegression()
+		cross_val(lr_stan, X_stan, y)
+
+		lr_stan.fit(X_stan, y)
+		y_predicted = lr_stan.predict(X_stan)
+		title = 'Fitted against ture values'
+		ys = [[y, 'True'],[y_predicted, 'Fitted']]
+		scatter(x_plt, ys, title=title)
+	elif (option == 'iii'):
+		# f_regression and mutual info regression 
+		F, p = f_regression(X,y)
+		mi = mutual_info_regression(X,y)
+		print (F)
+		print (mi)
+
+		X_reg = SelectKBest(f_regression, k=3).fit_transform(X,y)
+		X_mi = SelectKBest(mutual_info_regression, k=3).fit_transform(X,y)
+
+		lr_reg = linear_model.LinearRegression()
+		cross_val(lr_reg, X_reg, y)
+
+		lr_mi = linear_model.LinearRegression()
+		cross_val(lr_mi, X_mi, y)
 
 
 if __name__ == '__main__':
 	#Q1('a')
-	Q1('b')
-	#Q2a()
+	#Q1('b')
+	Q2a('iii')
